9999fb50774635f1bf211663a9f540ff0ccce7f9
tiko35 <gtgtigran35@gmail.com>
2016-09-30 18:06:09 +0400

    Edit version

--- lightbox.php
+++ lightbox.php
@@ -3,7 +3,7 @@
 Plugin Name: Huge IT Lightbox
 Plugin URI: http://huge-it.com/lightbox
 Description: Lightbox is the perfect tool for viewing photos.
-Version: 1.6.6
+Version: 1.6.7
 Author: Huge-IT
 Author URI: http://huge-it.com
 License: GPL
